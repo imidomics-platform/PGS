c492b2227f63a2683017ecb30c6861197246f352
Adrià Aterido <adria.aterido@imidomics.com>
2026-04-16 17:54:18 +0200

    pgs & bulk test

--- bio_worker/1.BulkTranscriptomics.R
+++ bio_worker/1.BulkTranscriptomics.R
@@ -46,42 +46,20 @@ volume_dir <- Sys.getenv("VOLUME_DIR", unset = "/media/bioinformatics/imidomics_
 # ===================================
 
 disease_of_interest <- "SS"
-tissue <- "blood"
+tissue <- "salivary_gland"
 
-assay1 <- "genetics"
-project1 <- "ssad_expanded"
+assay <- "singlecell"
+project <- "SSAD"
 
-assay2 <- "transcriptomics"
-project2 <- "SSAD"
-
-analysis <- "a2"
+analysis <- "a3"
 
 excl.lowexp<-"no"
 
-pseudo_bulk<-"no"
+pseudo_bulk<-"yes"
 
-# 1) Getting pgs dataset & genetic pc information for the specified disease, assay type, tissue and project
+# 1) Getting bulk transcriptomics dataset for the specified disease, assay type, tissue and project
 
-dataset_path <- list.files(paste0("/media/bioinformatics/imidomics_platform/volume_migration/tmp/analyses/",analysis), pattern = "hg", full.names = TRUE)
-ths.all<-readRDS(paste0(dataset_path,"/PGSprofiles/ThresholdingSummary.rds"))
-ths.opt<-ths.all[!is.na(ths.all$Nagelkerke.R2.All.FullDataset),"Threshold"]
-
-dataset_genetics_pgs <- read.table(paste0(dataset_path,"/PGSprofiles/",sub("^LD0?(\\d)\\.(P\\d+)$","PGS_R2_0.\\1.\\2.profile",ths.opt)),header=T)
-
-dataset_info <- list_datasets(list(disease = disease_of_interest, dataset_project=project1, assay = assay1), limit = 50)
-dataset_path    <- paste0(volume_dir, "/", dataset_info$dataset_path[1]) 
-dataset_version <- dataset_info$dataset_version[1]
-dataset_yaml_path <- paste0(dataset_path, "/", dataset_version, ".yaml")
-dataset_yaml <- yaml::read_yaml(dataset_yaml_path)
-
-dataset_genetics_pcs<-read.delim(paste0(dataset_path, "/", dataset_yaml$pc_data),header=T)
-dataset_genetics_samples<-read.delim(paste0(dataset_path, "/samples/samples.tsv"),header=T)
-
-print(paste0(disease_of_interest," | ",assay1," | ",project1," | ",dataset_info$dataset_name))
-
-# 2) Getting molecular dataset information for the specified disease, assay type, tissue and project
-
-dataset_info <- list_datasets(list(disease = disease_of_interest, dataset_project=project2, assay=assay2, tissue=tissue), limit = 50)
+dataset_info <- list_datasets(list(disease = disease_of_interest, dataset_project=project, assay=assay, tissue=tissue), limit = 50)
 dataset_path    <- paste0(volume_dir, "/", dataset_info$dataset_path[1]) 
 dataset_version <- dataset_info$dataset_version[1]
 dataset_yaml_path <- paste0(dataset_path, "/", dataset_version, ".yaml")
@@ -97,50 +75,66 @@ dataset_tech_mol<-read.delim(paste0(dataset_path, "/samples/samples_technology.t
 
 print(paste0(disease_of_interest," | ",assay2," | ",project2," | ",tissue," | ",dataset_info$dataset_name))
 
-# 3) Getting reference files
+# 2) Getting reference files
 
 gene.anno<-read.csv(paste0(volume_dir,"/reference/gene_reference_data/gene_annotation_hg38v44_hg19v19.tsv"),sep="\t")
 transc.feat<-readRDS("/media/bioinformatics/Projects/Internal_Projects/2024_IMX_PlatformUpdate/InputData/Internal/GenExp_hg38_featureTable.rds") # project-specific feature table
 #transc.feat<-readRDS(paste0(volume_dir,"/reference/gene_reference_data/hg38_GenExp_featureTable.rds"))
 
 # =================================================
-# mPGS Calculation 
+# DGEA
 # =================================================
 
 tmp.folder<-paste0(paste0("/media/bioinformatics/imidomics_platform/volume_migration/tmp/analyses/",analysis))
+dir.create(tmp.folder)
 
 #---------------- Merging Datasets -------------#
 
-df.gen.v0<-merge(dataset_genetics_pgs[,c("IID","SCORE")],dataset_genetics_samples[,c("sample_id","followup_id")], by.x="IID", by.y="sample_id")
-df.gen.v1<-merge(df.gen.v0,dataset_genetics_pcs[,c("sample_id","PC1","PC2")], by.x="IID", by.y="sample_id")
-df.gen<-df.gen.v1[,c(3:5,2)]
-colnames(df.gen)<-c("followup_id","gen.pc1","gen.pc2","pgs")
 if (tissue=="skin" | tissue=="salivary_gland") {
-  df.mol.samples<-merge(dataset_samples_mol[dataset_samples_mol$timepoint=="wk0" & dataset_samples_mol$tissue_type=="affected" & dataset_samples_mol$disease==disease_of_interest,c("sex","age","timepoint","sample_id","followup_id")], dataset_tech_mol[,c("sample_id","Sequencing_batch")], by="sample_id")
+  df.mol.cases<-merge(dataset_samples_mol[(dataset_samples_mol$timepoint=="wk0") & dataset_samples_mol$tissue_type=="affected", c("sex","age","timepoint","sample_id","followup_id","disease")], dataset_tech_mol[,c("sample_id","Sequencing_batch")], by="sample_id")
+  df.mol.ctrls<-merge(dataset_samples_mol[(dataset_samples_mol$timepoint=="uv") & dataset_samples_mol$tissue_type=="unaffected", c("sex","age","timepoint","sample_id","followup_id","disease")], dataset_tech_mol[,c("sample_id","Sequencing_batch")], by="sample_id")
+  df.mol.samples<-rbind(df.mol.cases,df.mol.ctrls)
 } else {
-  df.mol.samples<-merge(dataset_samples_mol[dataset_samples_mol$timepoint=="wk0" & dataset_samples_mol$disease==disease_of_interest,c("sex","age","timepoint","sample_id","followup_id")], dataset_tech_mol[,c("sample_id","Sequencing_batch")], by="sample_id")
-  
+  df.mol.samples<-merge(dataset_samples_mol[(dataset_samples_mol$timepoint=="wk0" | dataset_samples_mol$timepoint=="uv"),c("sex","age","timepoint","sample_id","followup_id","disease")], dataset_tech_mol[,c("sample_id","Sequencing_batch")], by="sample_id")
 }
-df.all.samples<-merge(df.mol.samples,df.gen,by="followup_id")
 
 mol.data<-as.data.frame(t(dataset_data_mol))
 if (excl.lowexp=="yes") {genes2analyze<-rownames(subset(transc.feat,ExpressedPercentageCategory_Disc != "Non-expressed" & ExpressedPercentageCategory_Repl != "Non-expressed"))} else {genes2analyze<-grep("ENS",colnames(mol.data),value=T)}
 mol.data.filt<-mol.data[colnames(mol.data) %in% genes2analyze]
 
-d2a<-merge(df.all.samples, mol.data.filt, by.x="sample_id", by.y=0)
-d2a$pgs<-as.numeric(scale(d2a$pgs))
+d2a<-merge(df.mol.samples, mol.data.filt, by.x="sample_id", by.y=0)
 d2a$Sequencing_batch<-as.factor(d2a$Sequencing_batch)
 d2a$sex<-as.factor(d2a$sex)
+d2a$IMID.num <- ifelse(d2a$disease == disease_of_interest, 1, 0)
+
 
 #---------------- Association Analysis ----#
 
 results <- lapply(genes2analyze, function(gene) {
-  formula <- as.formula(paste(gene, "~ pgs + gen.pc1 + gen.pc2 + age + sex + Sequencing_batch"))
-  model <- lm(formula, data=d2a)
-  out <- c(gene,coefficients(summary(model))["pgs","Estimate"],coefficients(summary(model))["pgs","Std. Error"],coefficients(summary(model))["pgs","Pr(>|t|)"])
+  formula <- as.formula(paste0("IMID.num ~ `", gene, "` + age + sex + Sequencing_batch"))
+  model <- glm(formula, data = d2a, family = "binomial")
+  
+  coefs <- coefficients(summary(model))
+  
+  if (!(gene %in% rownames(coefs))) {
+    message("Missing coefficient for: ", gene)
+    return(data.frame(
+      gene = gene,
+      Estimate = NA_real_,
+      Std.Error = NA_real_,
+      P.value = NA_real_
+    ))
+  }
+  
+  data.frame(
+    gene = gene,
+    Estimate = coefs[gene, "Estimate"],
+    Std.Error = coefs[gene, "Std. Error"],
+    P.value = coefs[gene, "Pr(>|z|)"]
+  )
 })
+res <- do.call(rbind, results)
 
-res<-as.data.frame(do.call(rbind, results))
 colnames(res)<-c("EnsemblID","Effect","Std.Error","Pvalue")
 res$Effect<-as.numeric(res$Effect); res$Pvalue<-as.numeric(res$Pvalue); res$Std.Error<-as.numeric(res$Std.Error);  res$TechID<-NA
 res<-merge(res,gene.anno[,c("EnsemblID","gene_symbol")],by="EnsemblID",all.x=T)
@@ -148,5 +142,5 @@ res<-res[,c(1,6,5,2:4)]
 res<-res[order(res$Pvalue),]
 res$FDR<-p.adjust(res$Pvalue, method="fdr")
 if (pseudo_bulk=="yes") {assay2<-"transcriptomics"} 
-saveRDS(res,paste0(tmp.folder,"/",disease_of_interest,"_PGS_",assay2,"_",tissue,".rds"))
+saveRDS(res,paste0(tmp.folder,"/",disease_of_interest,"_BulkTranscriptomics_",assay2,"_",tissue,".rds"))
 dim(res)
